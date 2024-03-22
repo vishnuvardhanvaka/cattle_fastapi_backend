68619a40e6e354fa6a02b9059777c10f828c04d6
vishnu <vishnu.vaka@solomonsint.com>
2024-03-22 19:15:28 +0530

    added a post path

--- app/api.py
+++ app/api.py
@@ -34,7 +34,24 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+#List of Diseases (26 Cattle Diseases Mention) is listed in list disease.
+disease=['mastitis','blackleg','bloat','coccidiosis','cryptosporidiosis',
+        'displaced_abomasum','gut_worms','listeriosis','liver_fluke','necrotic_enteritis','peri_weaning_diarrhoea',
+        ' rift_valley_fever','rumen_acidosis',
+        'traumatic_reticulitis','calf_diphtheria','foot_rot','foot_and_mouth','ragwort_poisoning','wooden_tongue','infectious_bovine_rhinotracheitis',
+'acetonaemia','fatty_liver_syndrome','calf_pneumonia','schmallen_berg_virus','trypanosomosis','fog_fever']
+
+
 # model = load('./app/KNN.joblib')
 with open('KNN.pkl', 'rb') as f:
     test_model = pickle.load(f)
 
+@app.post('/getDisease')
+def getDisease(symptoms: list):
+    # test_samplex=[[0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+    #actual output for above sample x is  disease[25] = fog_fever
+    predict = test_model.predict(symptoms)
+    predicted=predict[0]
+    print(predicted)
+    print(test_model[predicted])
+    return {'success':True,'disease':test_model[predicted]}
